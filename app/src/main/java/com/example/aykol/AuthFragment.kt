f9c3400c6e11d99ae19444b07f8217b60a4f3832
aycholpon-bektenbekova <aicholpon25t@gmail.com>
2023-06-17 00:41:43 +0600

    авторизация через персональный номер

---
+++ app/src/main/java/com/example/aykol/AuthFragment.kt
@@ -0,0 +1,45 @@
+package com.example.aykol
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import com.example.aykol.databinding.FragmentAuthBinding
+
+class AuthFragment : Fragment() {
+
+    private lateinit var binding: FragmentAuthBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentAuthBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        click()
+    }
+
+    private fun personNumberCheck() {
+        if (binding.personNumber.minEms != 10) {
+            Toast.makeText(requireContext(), getString(R.string._10), Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun click() {
+        binding.btnNext.setOnClickListener {
+            personNumberCheck()
+            if (binding.personNumber.text.isNotEmpty() ||
+                binding.password.text.isNotEmpty()
+            ) {
+                findNavController().navigate(R.id.navigation_home)
+            } else Toast.makeText(requireContext(), getString(R.string.inputs), Toast.LENGTH_SHORT).show()
+        }
+    }
+}
